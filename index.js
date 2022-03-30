7aa8a990e192ef1553030986644acad65e5ad875
akshitkumar8077 <akshitgupta150@gmail.com>
2022-03-30 22:20:15 +0530

    init

---
+++ index.js
@@ -0,0 +1,90 @@
+const dateElement = document.getElementById("date");
+
+dateElement.addEventListener("input", (e) => {
+  // console.log(e.target.value);
+});
+
+function makeRequest(data) {
+  // it makes a post fetch call
+  const url = "https://jsonplaceholder.typicode.com/posts";
+
+  const promise = fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header
+  });
+
+  promise.then((j) => j.json()).then((r) => console.log("-- response -- ", r));
+}
+
+/**
+ * 
+checks if 
+const invalidNames = [
+"Winter Soldier", "Iron Man", 
+"Black Widow", "Loki"
+];
+
+ */
+function isInvalidName(name) {
+  const invalidNames = ["Winter Soldier", "Iron Man", "Black Widow", "Loki"];
+
+  if (invalidNames.indexOf(name) > -1) {
+    return true;
+  }
+
+  return false;
+}
+
+// get the form element from the dom
+
+const formElement = document.getElementById("booking-form"); // write code to get the form element;
+
+// add event lisner to the form
+formElement.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("Form submigtted");
+
+  const data = {
+    fullName: formElement.elements["fullName"].value,
+    email: formElement.elements["email"].value,
+    doctor: formElement.elements["doctor"].value,
+    location: formElement.elements["location"].value,
+    date: formElement.elements["date"].value,
+  };
+
+  if (isInvalidName(data.fullName)) {
+    // show alert
+    window.alert("SOmething went wrong");
+  } else {
+    makeRequest(data);
+  }
+});
+
+// add event listner
+
+const nameElement = document.getElementById("fullName");
+const emailElement = document.getElementById("email");
+
+function storeElementInLocalStorage(key, value) {
+  localStorage.setItem(key, value);
+}
+
+nameElement.addEventListener("blur", (e) => {
+  storeElementInLocalStorage("fullName", e.target.value);
+});
+
+emailElement.addEventListener("blur", (e) => {
+  storeElementInLocalStorage("email", e.target.value);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  // fill back the values from localstorage
+  const fullName = localStorage.getItem("fullName");
+
+  const email = localStorage.getItem("email");
+
+  console.log(fullName, email);
+});
